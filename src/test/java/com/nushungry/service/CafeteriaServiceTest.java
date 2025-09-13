11f4ecb67dc974a19c7d20f94b7dc4c3270d5b5a
Shirley <chowjss@gmail.com>
2025-09-13 12:47:43 +0800

    Add unit and integration tests

---
+++ src/test/java/com/nushungry/service/CafeteriaServiceTest.java
@@ -0,0 +1,34 @@
+package com.nushungry.service;
+
+import com.nushungry.model.Cafeteria;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class CafeteriaServiceTest {
+    @Autowired
+    private CafeteriaService cafeteriaService;
+
+    @Test
+    void contextLoads() throws Exception {
+        assertThat(cafeteriaService).isNotNull();
+    }
+    @Test
+    void saveCafeteria() throws Exception {
+        Cafeteria cafeteria = new Cafeteria();
+        cafeteria.setName("Cafeteria");
+        cafeteria.setDescription("Description");
+        cafeteria.setLongitude(90);
+        cafeteria.setLatitude(90);
+        cafeteria.setLocation("Building 9201921");
+        cafeteria.setNearestBusStop("Biz 2");
+        assertThat(cafeteriaService.save(cafeteria)).isNotNull();
+    }
+    @Test
+    void findAllCafeterias() {
+        assertThat(cafeteriaService.findAll()).isNotEmpty();
+    }
+}
